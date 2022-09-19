7185e104191286c4d406c6667811b7e2a0a29611
Miriam Smith <mimsy.smith@yahoo.com>
2022-09-19 18:31:41 +1000

    Added type checks, error handling

--- main.py
+++ main.py
@@ -16,16 +16,27 @@ print("Let's get on with it then!")
 print()
 
 cont = True
-
+number = True
 while cont:
-
+    rating = ""
     name = input('Enter a symptom name. \n')
-    rating = int(input('How bad is that symptom today out of 10? 1 is best, 10 is worst. \n'))
+    
+    while number: 
+        rating = input('How bad is that symptom today out of 10? 1 is best, 10 is worst. \n')
+        if rating.isdigit():
+            rating = int(rating)
+            number = False
+        else:
+            print("Please enter a positive, whole number only")
+            number = True   
     notes = input('Finally, enter any notes you want to remember about this. If you can\'t think of any, just type \'pass\' \n')
+    
 
     dbhelp.insert_info(name, rating, notes)
 
     tocont = input("Do you have any other symptoms to enter? y/n \n")
+    tocont = tocont[0]
+    tocont = tocont.lower()
     if tocont == 'y':
         cont = True
     else:
@@ -34,6 +45,10 @@ while cont:
 print('\n' + '\n')
 
 out = input('Would you like to create an output file for your doctor? y/n \n')
+
+out = out[0]
+out = out.lower()
+
 if out == 'y':
     final = output.create_output()
     f = open('output.txt', 'a')
@@ -47,11 +62,16 @@ else:
 
 lst = input("Would you like to see a list of all your symptoms? y/n \n")
 
+lst = lst[0]
+lst = lst.lower()
+
 if lst == 'y':
     names = dbhelp.ret_names()
     print(str(names))
 
 delete = input("Do you need to delete any symptoms? y/n \n")
+delete = delete[0]
+delete = delete.lower()
 exit = True
 if delete == 'y':
     while exit:
@@ -63,17 +83,28 @@ if delete == 'y':
         else:
             exit = False
 
-ratlist = []
-finalrat = True
-while finalrat:
-    rat = input("Please enter the name of one of your worst symptoms \n" )
-    ratlist.append(rat)
-    fratexit = input("Are any other of your symptoms bad right now? y/n \n")
-    if fratexit == 'y':
-        finalrat = True
-    else:
-        finalrat = False
 
-print("We're going to calculate how you're going now. Hold on.")
-print("_"*20)
-print(dbhelp.overall_rating(ratlist))
+getrating = input("If you have built up a history of ratings, I can give you a general idea of how you're trevelling. Would that be useful right now?")
+getrating = getrating[0]
+getrating=getrating.lower()
+
+if getrating == 'y':
+    ratlist = []
+    finalrat = True
+    while finalrat:
+        rat = input("Please enter the name of one of your worst symptoms \n" )
+        ratlist.append(rat)
+        fratexit = input("Are any other of your symptoms bad right now? y/n \n")
+        fratexit=fratexit[0]
+        fratexit.lower()
+        if fratexit == 'y':
+            finalrat = True
+        else:
+            finalrat = False
+
+    print("We're going to calculate how you're going now. Hold on.")
+    print("_"*20)
+    print(dbhelp.overall_rating(ratlist))
+
+else:
+    print("Well, that's it from me for now. Cheery-bye!")
